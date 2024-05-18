2a68f76006175851e8dea42ee173b3675b841911
Jay Mistry <jayu412@gmail.com>
2024-05-18 13:32:45 +0530

    Added FizzBuzz program.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,138 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This program prints FizzBuzz word.");
+
+int limit = 100;
+
+PrintFizzBuzz(limit);
+
+PrintDivisibleBy(limit);
+
+PrintAllNumbersDivisibleByThree(limit);
+PrintAllNumbersDivisibleByFive(limit);
+PrintAllNumbersDivisibleByThreeAndFive(limit);
+
+static double DivisionOperation(int number, int divideBy)
+{
+    return (double)number % divideBy;
+}
+
+//print number from 1 to 1000
+//if number is Divisible by 3
+//print "Divisible by 3"
+//if number is Divisible by 5
+//print "Divisible by 5"
+//but if number is Divisible by 3 and 5 both 
+//print "Divisible by 3 and 5 both"(Do not show divisible by 3 or divisible by 5)
+//if number is not Divisible by 3 and 5
+//print "the number it self"
+static void PrintDivisibleBy(int limit)
+{
+    Console.WriteLine($"\nPrint Divisible from 1 to {limit} \n");
+
+    for (int number = 1; number <= limit; number++)
+    {
+        bool isDivisibleByThree = DivisionOperation(number, 3) == 0;
+        bool isDivisibleByFive = DivisionOperation(number, 5) == 0;
+
+        if (isDivisibleByThree) //10 % 3 = 0.4 => false
+        {
+            if (isDivisibleByFive) ///15 % 5 = 3 => true
+            {
+                Console.WriteLine($"Number is {number} Divisible by 3 and 5");
+            }
+            else
+            {
+                Console.WriteLine($"Number is {number} Divisible by 3");
+            }
+        }
+        else if (isDivisibleByFive) ///10 % 5 = 2 => true
+        {
+            Console.WriteLine($"Number is {number} Divisible 5");
+        }
+        else
+        {
+            Console.WriteLine($"Number is {number}");
+        }
+
+    }
+}
+
+//print number from 1 to 1000
+//if number is Divisible by 3
+//print "Divisible by 3"
+//if number is Divisible by 5
+//print "Divisible by 5"
+//but if number is Divisible by 3 and 5 both 
+//print "Divisible by 3 and 5 both"(Do not show divisible by 3 or divisible by 5)
+//if number is not Divisible by 3 and 5
+//print "the number it self"
+static void PrintFizzBuzz(int limit)
+{
+    Console.WriteLine($"\nPrint FizzBuzz from 1 to {limit} \n");
+
+    for (int number = 1; number <= limit; number++)
+    {
+        bool isDivisibleByThree = DivisionOperation(number, 3) == 0;
+        bool isDivisibleByFive = DivisionOperation(number, 5) == 0;
+
+        // Check if i is divisible by both 3 and 5 
+        if (isDivisibleByThree && isDivisibleByFive)
+            // Add "FizzBuzz" to the console log
+            Console.WriteLine($"{number} is FizzBuzz");
+        // Check if i is divisible by 3 
+        else if (isDivisibleByThree)
+            // Add "Fizz" to the console log
+            Console.WriteLine($"{number} is Fizz");
+        // Check if i is divisible by 5 
+        else if (isDivisibleByFive)
+            // Add "Buzz" to the console log
+            Console.WriteLine($"{number} is Buzz");
+        else
+            Console.WriteLine($"{number}");
+
+    }
+
+    Console.WriteLine("");
+}
+
+static void PrintAllNumbersDivisibleByThree(int limit)
+{
+    Console.WriteLine($"\nPrint numbers divided by 3, from 1 to {limit} \n");
+    for (int number = 1; number <= limit; number++)
+    {
+        if (number % 3 == 0) //10 % 3 = 0.4 => false
+        {
+            Console.Write($"{number}, ");
+        }
+    }
+
+    Console.WriteLine("");
+}
+
+static void PrintAllNumbersDivisibleByFive(int limit)
+{
+    Console.WriteLine($"\nPrint numbers divided by 5, from 1 to {limit} \n");
+    for (int number = 1; number <= limit; number++)
+    {
+        if (number % 5 == 0) //15 % 5 = 0 => true
+        {
+            Console.Write($"{number}, ");
+        }
+    }
+
+    Console.WriteLine("");
+}
+
+static void PrintAllNumbersDivisibleByThreeAndFive(int limit)
+{
+    Console.WriteLine($"\nPrint numbers divided by both 3 and 5, from 1 to {limit} \n");
+    for (int number = 1; number <= limit; number++)
+    {
+        if (number % 3 == 0 && number % 5 == 0) //15 % 5 = 0 => true
+        {
+            Console.Write($"{number}, ");
+        }
+    }
+
+    Console.WriteLine("");
+}
--- README.md
+++ README.md
@@ -1,2 +1,224 @@
+
 # fizzbuzz
 This program prints FizzBuzz word.
+
+Programming language is c#.
+
+Print FizzBuzz from 1 to 100
+
+1
+2
+3 is Fizz
+4
+5 is Buzz
+6 is Fizz
+7
+8
+9 is Fizz
+10 is Buzz
+11
+12 is Fizz
+13
+14
+15 is FizzBuzz
+16
+17
+18 is Fizz
+19
+20 is Buzz
+21 is Fizz
+22
+23
+24 is Fizz
+25 is Buzz
+26
+27 is Fizz
+28
+29
+30 is FizzBuzz
+31
+32
+33 is Fizz
+34
+35 is Buzz
+36 is Fizz
+37
+38
+39 is Fizz
+40 is Buzz
+41
+42 is Fizz
+43
+44
+45 is FizzBuzz
+46
+47
+48 is Fizz
+49
+50 is Buzz
+51 is Fizz
+52
+53
+54 is Fizz
+55 is Buzz
+56
+57 is Fizz
+58
+59
+60 is FizzBuzz
+61
+62
+63 is Fizz
+64
+65 is Buzz
+66 is Fizz
+67
+68
+69 is Fizz
+70 is Buzz
+71
+72 is Fizz
+73
+74
+75 is FizzBuzz
+76
+77
+78 is Fizz
+79
+80 is Buzz
+81 is Fizz
+82
+83
+84 is Fizz
+85 is Buzz
+86
+87 is Fizz
+88
+89
+90 is FizzBuzz
+91
+92
+93 is Fizz
+94
+95 is Buzz
+96 is Fizz
+97
+98
+99 is Fizz
+100 is Buzz
+
+
+Print Divisible from 1 to 100
+
+Number is 1
+Number is 2
+Number is 3 Divisible by 3
+Number is 4
+Number is 5 Divisible 5
+Number is 6 Divisible by 3
+Number is 7
+Number is 8
+Number is 9 Divisible by 3
+Number is 10 Divisible 5
+Number is 11
+Number is 12 Divisible by 3
+Number is 13
+Number is 14
+Number is 15 Divisible by 3 and 5
+Number is 16
+Number is 17
+Number is 18 Divisible by 3
+Number is 19
+Number is 20 Divisible 5
+Number is 21 Divisible by 3
+Number is 22
+Number is 23
+Number is 24 Divisible by 3
+Number is 25 Divisible 5
+Number is 26
+Number is 27 Divisible by 3
+Number is 28
+Number is 29
+Number is 30 Divisible by 3 and 5
+Number is 31
+Number is 32
+Number is 33 Divisible by 3
+Number is 34
+Number is 35 Divisible 5
+Number is 36 Divisible by 3
+Number is 37
+Number is 38
+Number is 39 Divisible by 3
+Number is 40 Divisible 5
+Number is 41
+Number is 42 Divisible by 3
+Number is 43
+Number is 44
+Number is 45 Divisible by 3 and 5
+Number is 46
+Number is 47
+Number is 48 Divisible by 3
+Number is 49
+Number is 50 Divisible 5
+Number is 51 Divisible by 3
+Number is 52
+Number is 53
+Number is 54 Divisible by 3
+Number is 55 Divisible 5
+Number is 56
+Number is 57 Divisible by 3
+Number is 58
+Number is 59
+Number is 60 Divisible by 3 and 5
+Number is 61
+Number is 62
+Number is 63 Divisible by 3
+Number is 64
+Number is 65 Divisible 5
+Number is 66 Divisible by 3
+Number is 67
+Number is 68
+Number is 69 Divisible by 3
+Number is 70 Divisible 5
+Number is 71
+Number is 72 Divisible by 3
+Number is 73
+Number is 74
+Number is 75 Divisible by 3 and 5
+Number is 76
+Number is 77
+Number is 78 Divisible by 3
+Number is 79
+Number is 80 Divisible 5
+Number is 81 Divisible by 3
+Number is 82
+Number is 83
+Number is 84 Divisible by 3
+Number is 85 Divisible 5
+Number is 86
+Number is 87 Divisible by 3
+Number is 88
+Number is 89
+Number is 90 Divisible by 3 and 5
+Number is 91
+Number is 92
+Number is 93 Divisible by 3
+Number is 94
+Number is 95 Divisible 5
+Number is 96 Divisible by 3
+Number is 97
+Number is 98
+Number is 99 Divisible by 3
+Number is 100 Divisible 5
+
+Print numbers divided by 3, from 1 to 100
+
+3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99,
+
+Print numbers divided by 5, from 1 to 100
+
+5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,
+
+Print numbers divided by both 3 and 5, from 1 to 100
+
+15, 30, 45, 60, 75, 90,
